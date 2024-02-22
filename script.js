ab5d62c65a8642d49cb7361979b7e53da6213e31
valentin01zz <117602648+valentin01zz@users.noreply.github.com>
2024-02-22 10:57:30 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('alertModal'), {
+        keyboard: false
+    });
+    myModal.show();
+});
